bea02b4e85873dca37f885ef67c2c16d9560f79d
LeoDivine <divineonyi2004@gmail.com>
2025-11-10 17:28:24 +0100

    feat: enhance typing indicator in Chatroom with user display pictures and improved message

--- src/components/pages/user/chats/chatroom/typing-watch-bar.tsx
+++ src/components/pages/user/chats/chatroom/typing-watch-bar.tsx
@@ -1,45 +1,49 @@
 import Image from "next/image";
 import React from "react";
 
-export default function TypingWatchBar() {
+export default function TypingWatchBar({
+  typingUsers,
+}: {
+  typingUsers: Map<
+    string,
+    {
+      name: string;
+      timestamp: number;
+      displayPicture?: string | null | undefined;
+    }
+  >;
+}) {
   return (
-    <div className=" absolute z-30   w-full mb-[70px] py-[10px]  bg-[#000000d5]  bottom-0 left-0 ">
-      <div className=" flex items-center px-[10px]  gap-3 ">
-        <div className=" flex items-center">
-          <Image
-            src={"/no-profile.png"}
-            alt="image"
-            width={"20"}
-            height={"20"}
-            className=" border border-black rounded-full"
-          />
-          <Image
-            src={"/no-profile.png"}
-            alt="image"
-            width={"20"}
-            height={"20"}
-            className="border border-black -ml-[10px] rounded-full"
-          />
-          <Image
-            src={"/no-profile.png"}
-            alt="image"
-            width={"20"}
-            height={"20"}
-            className="border border-black -ml-[10px] rounded-full"
-          />
-          <Image
-            src={"/no-profile.png"}
-            alt="image"
-            width={"20"}
-            height={"20"}
-            className="border border-black -ml-[10px] rounded-full"
-          />
-          <p className=" text-[12px]">+4</p>
+    <>
+      {typingUsers.size > 0 && (
+        <div className=" bg-black w-full py-[10px]  bottom-0 left-0 z-30 absolute mb-[71px] ">
+          <div className="flex items-center gap-2  text-zinc-400 text-sm animate-fade-in">
+            <div className="flex gap-1">
+              <div className="w-2 h-2 bg-zinc-400 rounded-full animate-bounce" />
+              <div
+                className="w-2 h-2 bg-zinc-400 rounded-full animate-bounce"
+                style={{ animationDelay: "0.1s" }}
+              />
+              <div
+                className="w-2 h-2 bg-zinc-400 rounded-full animate-bounce"
+                style={{ animationDelay: "0.2s" }}
+              />
+            </div>
+            <span>
+              <p>
+                {Array.from(typingUsers.values())[0].name}
+                {`${
+                  typingUsers.size > 1
+                    ? ` and ${typingUsers.size - 1} person is typing`
+                    : typingUsers.size > 1
+                    ? ` and ${typingUsers.size - 1} persons are typing`
+                    : " is typing"
+                } `}
+              </p>
+            </span>
+          </div>
         </div>
-        <div className=" text-accent text-[12px]">
-          <p>typing..</p>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
